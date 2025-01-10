df790c1ee3383054e295b281040e30bca68d3fe5
Mat45duv <mathisduvive@gmail.com>
2025-01-10 15:28:53 +0100

    13

--- word/modifier_article.php
+++ word/modifier_article.php
@@ -87,7 +87,7 @@ $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modifier un Article</title>
-    <link rel="stylesheet" href="style5.css">
+    <link rel="stylesheet" href="style6.css">
 </head>
 <body>
     <div class="container">
