e8bf0e6265333ddf2103b58bf67659a0a1eeee76
J0rdvn <jobafuidinsoni@stpbb.org>
2025-01-17 13:38:33 +0100

    menu deroulant

--- word/index.php
+++ word/index.php
@@ -35,21 +35,25 @@ require_once 'filtres.php';
     <!-- Menu déroulant pour PROJETS -->
     <div class="nav">
         <a href="#projet" onclick="toggleDropdown(event, 'dropdown-menu-projet')">PROJETS</a>
+        <?php if (isset($_SESSION['user_role']) && ($_SESSION['user_role'] == 'admin' )): ?>
         <div id="dropdown-menu-projet" class="dropdown-menu">
             <a href="ajouter_projet.php">Ajouter un Projet</a>
             <a href="suggestion.php">Suggestions de Projet</a>
             <a href="modifier_projet.php">Modifier un Projet</a>
             <a href="supprimer_projet.php">Supprimer un Projet</a>
         </div>
+            <?php endif; ?>
     </div>
 
     <!-- Menu déroulant pour BLOG -->
     <div class="nav">
         <a href="#blog" onclick="toggleDropdown(event, 'dropdown-menu-blog')">BLOG</a>
+        <?php if (isset($_SESSION['user_role']) && ($_SESSION['user_role'] == 'admin' )): ?>
         <div id="dropdown-menu-blog" class="dropdown-menu">
             <a href="ajouter_article.php">Ajouter un Article</a>
             <a href="modifier_article.php">Modifier un Article</a>
         </div>
+        <?php endif; ?>
     </div>
 
     <!-- Autres liens -->
@@ -262,8 +266,8 @@ document.addEventListener('click', function (event) {
         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
       </svg> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-      </svg> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-half" viewBox="0 0 16 16">
-        <path d="M5.354 5.119 7.538.792A.52.52 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.54.54 0 0 1 16 6.32a.55.55 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.5.5 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.6.6 0 0 1 .085-.302.51.51 0 0 1 .37-.245zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.56.56 0 0 1 .162-.505l2.907-2.77-4.052-.576a.53.53 0 0 1-.393-.288L8.001 2.223 8 2.226z"/>
+      </svg> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
       </svg>
 </div>
 
@@ -277,7 +281,7 @@ document.addEventListener('click', function (event) {
     aliquet a ipsum nec tincidunt. Nulla vitae rhoncus leo. Praesent dui sapien, bibendum quis tempus dictum.</p>
 
 <div class="profilauteur">
-    <img src="assets/images/lena.jpg" width="125px" height="125px">
+    <img src="https://studio-amelie-marzouk.com/wp-content/uploads/2023/07/AmelieMarzouk-Ariane-Cronel-37_pp-scaled.jpg" width="125px" height="125px">
 </div>
 
 <div class="auteur"
@@ -291,9 +295,7 @@ document.addEventListener('click', function (event) {
         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
       </svg> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-      </svg> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-half" viewBox="0 0 16 16">
-        <path d="M5.354 5.119 7.538.792A.52.52 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.54.54 0 0 1 16 6.32a.55.55 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.5.5 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.6.6 0 0 1 .085-.302.51.51 0 0 1 .37-.245zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.56.56 0 0 1 .162-.505l2.907-2.77-4.052-.576a.53.53 0 0 1-.393-.288L8.001 2.223 8 2.226z"/>
-      </svg>
+      </svg> 
 </div>
 
 </div>
@@ -306,7 +308,7 @@ document.addEventListener('click', function (event) {
     aliquet a ipsum nec tincidunt. Nulla vitae rhoncus leo. Praesent dui sapien, bibendum quis tempus dictum.</p>
 
 <div class="profilauteur">
-    <img src="assets/images/lena.jpg" width="125px" height="125px">
+    <img src="https://media.licdn.com/dms/image/v2/D4E03AQF05K6YjbdkBg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1731924206011?e=2147483647&v=beta&t=1xbRlKdDcvkcdLstpgQquhz8K7VTghuvpnJII-6CJ58" width="125px" height="125px">
 </div>
 
 <div class="auteur"
